d31a60e17a4eb17aa6eab372349d9434aeedf589
JoaoZati <joaozati@gmail.com>
2021-10-08 20:07:00 -0300

    fixing no text in orignal_text inssue

--- mysite/translator/views.py
+++ mysite/translator/views.py
@@ -6,6 +6,11 @@ from .translator import translate
 def translator_view(request):
     if request.method == 'POST':
         original_text = request.POST['my_textarea']
+        if not original_text:
+            return render(
+                request,
+                'translator.html',
+            )
         output_text = translate(original_text)
         return render(
             request,
