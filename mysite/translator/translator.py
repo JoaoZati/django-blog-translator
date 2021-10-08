641fce9747850ab0424ad11691032a6b131769ad
JoaoZati <joaozati@gmail.com>
2021-10-08 19:42:07 -0300

    Create function translator - Create a function that upper original text and print in the terminal. - next step is output this in the page

---
+++ mysite/translator/translator.py
@@ -0,0 +1,4 @@
+def translate(original_text):
+    output_text = original_text.upper()
+    print(output_text)
+    return output_text
--- mysite/translator/views.py
+++ mysite/translator/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .translator import translate
+
 
 def translator_view(request):
+    if request.method == 'POST':
+        original_text = request.POST['my_textarea']
+        output_text = translate(original_text)
+        return render(request, 'translator.html')
     return render(request, 'translator.html')
